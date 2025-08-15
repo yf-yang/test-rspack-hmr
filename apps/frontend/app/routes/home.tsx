e4643735974d3cf9f8be24b8bd7e3506dfa764b6
yf-yang <directoryyf@gmail.com>
2025-08-15 10:55:54 +0800

    feat: bump dependencies and reproduce

--- apps/frontend/app/routes/home.tsx
+++ apps/frontend/app/routes/home.tsx
@@ -1,43 +1,10 @@
-import createClient from 'openapi-fetch';
-import type { ReactNode } from 'react';
-import { MdOutlinePlayArrow } from 'react-icons/md';
+import { ReactNode } from "react";
 
-import type { paths } from '@/apis/schema';
-import { Button } from '@/components/shadcn-ui/button';
 
-import type { Route } from './+types/home';
-
-export function meta(_: Route.MetaArgs): Route.MetaDescriptors {
-  return [
-    { title: 'New React Router App' },
-    { name: 'description', content: 'Welcome to React Router!' },
-  ];
-}
-
-const client = createClient<paths>({ baseUrl: 'http://localhost:10086' });
-
-const logger = MAKE_LOGGER('example');
-
-export default function Home(): ReactNode {
+export default function ChatPage(): ReactNode {
   return (
-    <div className="flex size-full items-center justify-center">
-      <div>
-        <Button
-          variant="outline"
-          onClick={async () => {
-            INFO(logger, 'hello world');
-            const { data } = await client.POST('/echo_payload', {
-              body: {
-                message: 'hello',
-              },
-            });
-            INFO(logger, 'response: %o', data);
-          }}
-          className="mt-4 bg-red-500"
-        >
-          <MdOutlinePlayArrow className="mr-2 size-4" />
-        </Button>
-      </div>
+    <div className="flex h-screen w-screen bg-gray-50">
+      <div>Hello Worlds 123</div>
     </div>
   );
 }
